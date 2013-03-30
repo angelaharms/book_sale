dd0f8c4ce0be0c2db4fc587b3a0c81d651ae199d
Angela Harms <angela.harms@gmail.com>
2013-03-30 15:05:41 -0700

    Fix test names

--- spec/sale_spec.rb
+++ spec/sale_spec.rb
@@ -1,7 +1,7 @@
 require 'sale'
 
 describe Sale do
-  it "gives $0 for no cart" do
+  it "gives $0 for no books" do
     cart = []
     subject.total(cart).should == 0
   end
@@ -11,12 +11,12 @@ describe Sale do
     subject.total(cart).should == 8
   end
 
-  it "gives $15.20 for two different cart" do
+  it "gives $15.20 for two different books" do
     cart = [:one, :five]
     subject.total(cart).should == 15.20
   end
 
-  it "gives $21.60 for three different cart" do
+  it "gives $21.60 for three different books" do
     cart = [:one, :four, :five]
     subject.total(cart).should == 21.60
   end
