0fc6e5801216f45ff5baf186b968ed5984579b7e
Angela Harms <angela.harms@gmail.com>
2013-03-30 15:03:07 -0700

    books->cart

--- lib/sale.rb
+++ lib/sale.rb
@@ -5,9 +5,9 @@ class Sale
   FIVE_PERCENT_DISC = 0.95
   TEN_PERCENT_DISC = 0.9
 
-  def total(books)
-    cost = books.count * BOOK_PRICE
-    discount = case books.count
+  def total(cart)
+    cost = cart.count * BOOK_PRICE
+    discount = case cart.count
                when 2
                  FIVE_PERCENT_DISC
                when 3
--- spec/sale_spec.rb
+++ spec/sale_spec.rb
@@ -1,23 +1,23 @@
 require 'sale'
 
 describe Sale do
-  it "gives $0 for no books" do
-    books = []
-    subject.total(books).should == 0
+  it "gives $0 for no cart" do
+    cart = []
+    subject.total(cart).should == 0
   end
 
   it "gives $8 for one book" do
-    books = [:one]
-    subject.total(books).should == 8
+    cart = [:one]
+    subject.total(cart).should == 8
   end
 
-  it "gives $15.20 for two different books" do
-    books = [:one, :five]
-    subject.total(books).should == 15.20
+  it "gives $15.20 for two different cart" do
+    cart = [:one, :five]
+    subject.total(cart).should == 15.20
   end
 
-  it "gives $21.60 for three different books" do
-    books = [:one, :four, :five]
-    subject.total(books).should == 21.60
+  it "gives $21.60 for three different cart" do
+    cart = [:one, :four, :five]
+    subject.total(cart).should == 21.60
   end
 end
